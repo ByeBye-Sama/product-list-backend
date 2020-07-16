c087a1b4f92c68ee93e31274515ed8e1df994cbd
ByeBye-Sama <joaquin.byebye@gmail.com>
2020-07-16 01:53:32 -0500

    feat: add seeders

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -11,6 +12,17 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 15; $i++) {
+            Product::create([
+                'name' => $faker->name,
+                'description' => $faker->paragraph($nbSentences = 2, $variableNbSentences = true),
+                'category'=> $faker->word,
+                'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 99)
+            ]);
+        }
     }
 }
